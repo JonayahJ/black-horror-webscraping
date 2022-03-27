b9cd7b346219aa09a836f36f32a5fbd21df5d2dc
Jonayah Jackson <jonayah@thinkhalcyon.com>
2022-03-27 19:39:00 -0400

    setting up dependiencies

--- getinfo.py
+++ getinfo.py
@@ -1,12 +1,17 @@
 # Selenium
 # Google Chrome Web Driver
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = "/usr/local/bin/chromedriver"
 
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.thinkhalcyon.com")
+driver.get("https://www.wikipedia.com")
 
 print(driver.title)
 
